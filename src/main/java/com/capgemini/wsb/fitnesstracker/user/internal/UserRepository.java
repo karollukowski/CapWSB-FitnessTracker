cf7717700ad1563506618d850cacd67b3b949cae
Karol Łukowski <karollukowski12@gmail.com>
2024-05-20 21:07:20 +0200

    Cw 1.1

--- src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserRepository.java
+++ src/main/java/com/capgemini/wsb/fitnesstracker/user/internal/UserRepository.java
@@ -2,11 +2,20 @@ package com.capgemini.wsb.fitnesstracker.user.internal;
 
 import com.capgemini.wsb.fitnesstracker.user.api.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 
 interface UserRepository extends JpaRepository<User, Long> {
+
+    @Query("SELECT u FROM User u WHERE LOWER(u.email) = LOWER(:email)")
+    List<User> findByEmailIgnoreCase(@Param("email") String email);
+
+    @Query("SELECT u FROM User u WHERE LOWER(u.name) LIKE LOWER(CONCAT('%', :nameFragment, '%'))")
+    List<User> findByNameContainingIgnoreCase(@Param("nameFragment") String nameFragment);
 
     /**
      * Query searching users by email address. It matches by exact match.
